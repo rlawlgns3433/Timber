cdcc2527c78da37c5d7647e011739e30a42f9820
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2024-02-05 23:25:55 +0900

    RSA 키 암호화 작업중

--- Framework/Utils.h
+++ Framework/Utils.h
@@ -4,11 +4,27 @@
 
 class Utils
 {
-public : 
+private:
+	//static int p;
+	//static int q;
+	//static int n;
+	//static int z;
+	//static int e;
+	//static int d;
+
+public:
 	static void SetOrigin(sf::Transformable& obj, Origins originPreset, const sf::FloatRect& rect);
 	static void SetOrigin(sf::Sprite& obj, Origins originPreset);
 	static void SetOrigin(sf::Text& obj, Origins originPreset);
 	static void SetOrigin(sf::Shape& obj, Origins originPreset);
 	static sf::Vector2f GetRandomVector2(float start, float end);
 	static float GetRandomAngle();
+
+	//static int GetGCD(int a, int b);
+	//static int GetPrivateKeyExponent(int exponent, int z);
+	//static int Encrypt(int message, int exponent, int n);
+	//static int Decrypt(int cipherText, int exponent, int n);
+	//static bool IsPrime(int number);
+	//static int GenerateRandomPrime(int min = 2, int max = 200);
+	//static int GetPublicKeyExponent(int exponent);
 };
